0a8347e7946feea517c54a6fccf2b9644ef85384
Jmattgiroux <git config --global user.email jmattgiroux@gmail.comgit config --global user.email>
2020-12-11 01:37:12 -0600

    finished implementation of app, added screenshot and gif to repo, updated README

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # cs246-android-1
+
+# cs246-java-1
+
+Program Description: 
+
+This is an adaptation of the ChangeCalculator I created in Java.
+The ChangeCalculator is
+"A program that finds the change for a transaction using the smallest amount of coins.\
+The purpose of creating this program is to gain familiarity with the following\
+programming elements within Java:"
+
+- Variables (DONE)
+- Expressions (DONE)
+- Conditionals (DONE)
+- Loops (NOT DONE)
+- Functions (NOT DONE)
+- Classes (NOT DONE (unless you count the CalculateChange class))
+
+
+Development Environment: 
+
+Windows 10
+
+openjdk version "14.0.2" 2020-07-14\
+OpenJDK Runtime Environment (build 14.0.2+12-Ubuntu-120.04)\ 
+OpenJDK 64-Bit Server VM (build 14.0.2+12-Ubuntu-120.04, mixed mode, sharing)\
+
+Android Studio
+
+Build and Execution Instructions:\
+
+clone this git repository
+open the project in Android Studio
+run the application on an Android Virtual Device within Android Studio
+
+
+Screenshots:
+
+- This is a gif of the app running; it calculates change, and handles invalid input without crashing.
+
+![gif]()
+
+- This is a screenshot of a bug the app has; for some reason, subtracting the cost float from the cash float sometimes results in a penny being lost.
+
+![bug screenshot]()
+
+
+List of Useful Websites:
+
+- https://en.wikipedia.org/wiki/Java_(programming_language)
+- https://www.w3schools.com/java/default.asp
+- https://docs.oracle.com/en/java/javase/index.html
+- https://beginnersbook.com/java-collections-tutorials/
+
+
--- app/src/main/java/com/example/changecalculator/MainActivity.java
+++ app/src/main/java/com/example/changecalculator/MainActivity.java
@@ -3,9 +3,16 @@ package com.example.changecalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
+//    https://crunchify.com/how-to-create-simple-calculator-android-app-using-android-studio/
 
     float cost = 0;
     float cash = 0;
@@ -17,30 +24,71 @@ public class MainActivity extends AppCompatActivity {
     int nickels = 0;
     int pennies = 0;
 
+    Button calculate;
+
+    EditText inputCost, inputCash;
+
+    TextView result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        calculate = (Button) findViewById(R.id.CalculateButton);
+
+//        https://abhiandroid.com/ui/edittext
+        inputCost = (EditText) findViewById(R.id.CostField);
+        inputCash =  (EditText) findViewById(R.id.CashField);
+
+        result = (TextView) findViewById(R.id.Result);
+
         //get cash and cost via interface
 
 
 
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        //below code triggers on pressing calculate button
-        if (cash > cost)
-        {
-            change = cash - cost;
-            pennies = (int)(change * 100);
-            dollars = pennies / 100;
-            pennies -= dollars * 100;
-            quarters = pennies / 25;
-            pennies -= quarters * 25;
-            dimes = pennies / 10;
-            pennies -= dimes * 10;
-            nickels = pennies / 5;
-            pennies -= nickels * 5;
+//                https://stackoverflow.com/questions/10735679/how-to-convert-string-into-float-value-in-android/10735754
 
+                try{
+                    String string = inputCost.getText().toString();
+
+                    cost = Float.parseFloat(string);
+
+                    string = inputCash.getText().toString();
+
+                    cash = Float.parseFloat(string);
+                } catch (Exception e){
+                    result.setText("Please only input numbers");
+                }
+
+
+                //below code triggers on pressing calculate button
+                if (cash > cost)
+                {
+                    change = cash - cost;
+                    pennies = (int)(change * 100);
+                    dollars = pennies / 100;
+                    pennies -= dollars * 100;
+                    quarters = pennies / 25;
+                    pennies -= quarters * 25;
+                    dimes = pennies / 10;
+                    pennies -= dimes * 10;
+                    nickels = pennies / 5;
+                    pennies -= nickels * 5;
+
+                    String resultString;
+
+                    resultString = "Your change is: \n"
+                            + dollars + " Dollars\n"
+                            + quarters + " Quarters\n"
+                            + dimes + " Dimes\n"
+                            + nickels + " Nickels, and\n"
+                            + pennies + " Pennies\n";
            /* Display this
                 System.out.println("Your change is: \n"
                     + dollars + " Dollars\n"
@@ -48,13 +96,27 @@ public class MainActivity extends AppCompatActivity {
                     + dimes + " Dimes\n"
                     + nickels + " Nickels, and\n"
                     + pennies + " Pennies\n");
-
             */
-        }
-        else
-        {
-            // display System.out.println("The cash given cannot cover the cost\n");
-        }
+                    result.setText(resultString);
+
+                }
+                else
+                {
+
+                    result.setText("The cash given cannot cover the cost");
+                    // display System.out.println("The cash given cannot cover the cost\n");
+                }
+                //reset our values
+                cost = 0;
+                cash = 0;
+
+            }
+        });
+
+
+
+
+
 
     }
 }
